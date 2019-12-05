ced73869b0d4b570075def3ed79684c3fd55707c
Yu-sysbio <cheyu@chalmers.se>
2019-12-05 14:06:43 +0100

    New simulations

--- simulationCrabtree.m
+++ simulationCrabtree.m
@@ -35,7 +35,7 @@ f = tot_protein * f_modeled_protein;
 clear tot_protein f_modeled_protein;
 
 %% Solve LPs
-mu_list = 0.05:0.05:0.45;
+mu_list = 0.02:0.02:0.38;
 
 fluxes = zeros(length(model.rxns),length(mu_list));
 
@@ -73,13 +73,13 @@ co2 = fluxes(strcmp(model.rxns,'r_1672'),:);
 figure('Name','orginial');
 hold on;
 box on;
-plot(mu,glc,'-o','LineWidth',0.75,'Color',[55,126,184]/255);
-plot(mu,etoh,'-o','LineWidth',0.75,'Color',[255,127,0]/255);
-plot(mu,o2,'-o','LineWidth',0.75,'Color',[77,175,74]/255);
-plot(mu,co2,'-o','LineWidth',0.75,'Color',[152,78,163]/255);
+plot(mu,glc,'-o','MarkerSize',5,'LineWidth',0.75,'Color',[55,126,184]/255);
+plot(mu,etoh,'-o','MarkerSize',5,'LineWidth',0.75,'Color',[255,127,0]/255);
+plot(mu,o2,'-o','MarkerSize',5,'LineWidth',0.75,'Color',[77,175,74]/255);
+plot(mu,co2,'-o','MarkerSize',5,'LineWidth',0.75,'Color',[152,78,163]/255);
 % plot(mu,ac,'-o','LineWidth',0.75,'Color',[247,129,191]/255);
 % plot(mu,aldh,'-o','LineWidth',0.75,'Color',[247,129,191]/255);
-xlim([0 0.5]);
+xlim([0 0.4]);
 
 set(gca,'FontSize',12,'FontName','Helvetica');
 xlabel('Growth rate (/h)','FontSize',14,'FontName','Helvetica');
@@ -96,13 +96,13 @@ dummy = fluxes(strcmp(model.rxns,'dilute_dummy'),:)./mu; %g/gCDW
 figure('Name','dummy');
 hold on;
 box on;
-plot(mu,dummy,'-o','LineWidth',0.75,'Color',[82,82,82]/255);
-xlim([0 0.5]);
+plot(mu,dummy,'-o','MarkerSize',5,'LineWidth',0.75,'Color',[82,82,82]/255);
+xlim([0 0.4]);
 set(gca,'FontSize',12,'FontName','Helvetica');
 xlabel('Growth rate (/h)','FontSize',14,'FontName','Helvetica');
 ylabel('Dummy (g/gCDW)','FontSize',14,'FontName','Helvetica');
-set(gcf,'position',[0 0 240 185]);
-set(gca,'position',[0.17 0.2 0.76 0.5]);
+set(gcf,'position',[0 0 240 140]);
+set(gca,'position',[0.17 0.28 0.76 0.63]);
 clear;
 
 
