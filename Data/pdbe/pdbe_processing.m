697be439d4491e6b5d375c292c23027c3045c294
Yu-sysbio <cheyu@chalmers.se>
2020-01-05 17:43:06 +0100

    New codes

--- Data/pdbe/pdbe_processing.m
+++ Data/pdbe/pdbe_processing.m
@@ -59,7 +59,7 @@ pdb_processing_1.pdb_chains = cell(0,1);
 pdb_processing_1.all_pdb_chains = cell(0,1);
 
 for i = 1:length(pdb.pdbid)
-    disp(['Stage 1: ' num2str(i) '/' num2str(length(pdb.pdbid))]);
+    disp(['Processing raw data: ' num2str(i) '/' num2str(length(pdb.pdbid))]);
     gene_tmp = pdb.geneid{i};
     gene_list_tmp = strsplit(gene_tmp,'; ')';
     gene_list_tmp = unique(gene_list_tmp);
@@ -79,141 +79,143 @@ for i = 1:length(pdb.pdbid)
     end
 end
 save('pdb_processing_1.mat','pdb_processing_1');
-% 2. select gene-pdb pairs with highest blastp scores or highest coverage
+
+
+% 2. Collect cofactor information
+% For a protein with multiple pdb ids, all types of cofactors would be
+% integrated and the max copy number would be chosen.
 
 genes_collected = unique(pdb_processing_1.geneid);
 
-% % find the lowest identity and coverage
-% str_collected = cell(0,1);
-% for i = 1:length(genes_collected)
-%     disp(['Define cutoff of blastp: ' num2str(i) '/' num2str(length(genes_collected))]);
-%     gene_tmp = genes_collected(i);
-%     idx_tmp = ismember(pdb_processing_1.geneid,gene_tmp);
-%     pdbid_tmp = pdb_processing_1.pdbid(idx_tmp);
-%     pdbchain_tmp = pdb_processing_1.all_pdb_chains(idx_tmp);
-%     for k = 1:length(pdbid_tmp)
-%         pdbid_tmp_tmp = pdbid_tmp(k);
-%         pdbchain_tmp_tmp = split(pdbchain_tmp(k),'; ')';
-%         for j = 1:length(pdbchain_tmp_tmp)
-%             str_tmp = strcat(pdbid_tmp_tmp,'_',pdbchain_tmp_tmp(j),':',gene_tmp);
-%             str_collected = [str_collected;str_tmp];
-%         end
-%     end
-% end
-% str_all_pdb = strcat(txt(:,2),':',txt(:,1));
-% 
-% identity = num(:,1);
-% coverage = num(:,2);
-% evalue = num(:,3);
-% blastp_pdb = struct;
-% blastp_pdb.str_collected_list = str_collected;
-% blastp_pdb.identity_list = zeros(length(str_collected),1);
-% blastp_pdb.coverage_list = zeros(length(str_collected),1);
-% blastp_pdb.evalue_list = zeros(length(str_collected),1);
-% for i = 1:length(str_collected)
-%     disp(['Define cutoff of blastp: ' num2str(i) '/' num2str(length(str_collected))]);
-%     str_tmp = str_collected(i);
-%     if ismember(str_tmp,str_all_pdb)
-%         idx_tmp = ismember(str_all_pdb,str_tmp);
-%         identity_tmp = identity(idx_tmp);
-%         coverage_tmp = coverage(idx_tmp);
-%         evalue_tmp = evalue(idx_tmp);
-%         idx_tmp_tmp = find(evalue_tmp == min(evalue_tmp),1);
-%         blastp_pdb.identity_list(i) = identity_tmp(idx_tmp_tmp);
-%         blastp_pdb.coverage_list(i) = coverage_tmp(idx_tmp_tmp);
-%         blastp_pdb.evalue_list(i) = evalue_tmp(idx_tmp_tmp);
-%     else
-%         blastp_pdb.identity_list(i) = NaN;
-%         blastp_pdb.coverage_list(i) = NaN;
-%         blastp_pdb.evalue_list(i) = NaN;
-%     end
-% end
-% %%%%%%%%%%%%%%%%%%%%%%
-% min(blastp_pdb.identity_list);
-% min(blastp_pdb.coverage_list);
-% max(blastp_pdb.evalue_list);
-% quantile(blastp_pdb.identity_list,0.01,1);
-% quantile(blastp_pdb.coverage_list,0.01,1);
-% quantile(blastp_pdb.evalue_list,0.99,1);
-% %%%%%%%%%%%%%%%%%%%%%%
-
-pdb_processing_2 = struct();
-pdb_processing_2.pdbid = cell(0,1);
-pdb_processing_2.geneid = cell(0,1);
-% pdb_processing_2.name = cell(0,1);
-pdb_processing_2.cofactor = cell(0,1);
-% pdb_processing_2.peptide_entityid = zeros(0,1);
-pdb_processing_2.prot_stoic = zeros(0,1); % number of peptides per complex
-pdb_processing_2.cofactor_stoic = cell(0,1); % number of cofactors in the complex
-% pdb_processing_2.pdb_chains = cell(0,1);
-% pdb_processing_2.all_pdb_chains = cell(0,1);
-
-blastp_all_pdb = strcat(blastp.pdb,':',blastp.protein);
+pdb_cofactor = struct();
+pdb_cofactor.protein = cell(0,1);
+pdb_cofactor.cofactor_type = cell(0,1);
+pdb_cofactor.cofactor_copy = cell(0,1); % copy of cofactors per subunit/peptide
+pdb_cofactor.pdbid = cell(0,1);
 
 for i = 1:length(genes_collected)
-    disp(['Stage 2: ' num2str(i) '/' num2str(length(genes_collected))]);
+    disp(['Collecting cofactor: ' num2str(i) '/' num2str(length(genes_collected))]);
     gene_tmp = genes_collected(i);
     idx_tmp = ismember(pdb_processing_1.geneid,gene_tmp);
 	loc_tmp = find(idx_tmp);
     
-    if length(loc_tmp) == 1
-        pdb_processing_2.pdbid = [pdb_processing_2.pdbid;pdb_processing_1.pdbid(loc_tmp)];
-        pdb_processing_2.geneid = [pdb_processing_2.geneid;gene_tmp];
-        pdb_processing_2.cofactor = [pdb_processing_2.cofactor;pdb_processing_1.cofactor(loc_tmp)];
-        pdb_processing_2.prot_stoic = [pdb_processing_2.prot_stoic;pdb_processing_1.prot_stoic(loc_tmp)];
-        pdb_processing_2.cofactor_stoic = [pdb_processing_2.cofactor_stoic;pdb_processing_1.cofactor_stoic(loc_tmp)];
-    else
-        cmp_num_tmp = zeros(0,4);
-        cmp_txt_tmp = cell(0,1);
-        for j = 1:length(loc_tmp)
-            pdbid_tmp = pdb_processing_1.pdbid(loc_tmp(j));
-            pdbchain_tmp = pdb_processing_1.all_pdb_chains(loc_tmp(j));
-            pdbchain_tmp = split(pdbchain_tmp,'; ');
-            for k = 1:length(pdbchain_tmp)
-                str_tmp = strcat(pdbid_tmp,'_',pdbchain_tmp(k),':',gene_tmp);
-                if ismember(str_tmp,blastp_all_pdb)
-                    idx_blastp_tmp = ismember(blastp_all_pdb,str_tmp);
-                    n_tmp = length(find(idx_blastp_tmp));
-                    identity_tmp = blastp.identity(idx_blastp_tmp);
-                    coverage_tmp = blastp.coverage(idx_blastp_tmp);
-                    evalue_tmp = blastp.evalue(idx_blastp_tmp);
-                    bit_tmp = blastp.bit(idx_blastp_tmp);
-                    row_tmp = [identity_tmp coverage_tmp evalue_tmp bit_tmp];
-                    cmp_num_tmp = [cmp_num_tmp;row_tmp];
-                    cmp_txt_tmp = [cmp_txt_tmp;repelem(str_tmp,n_tmp)'];
-                else
-                    cmp_num_tmp = [cmp_num_tmp;[0 0 1000 0]];
-                    cmp_txt_tmp = [cmp_txt_tmp;str_tmp];
-                end
-            end
+    tmp_cofactor_type = cell(0,1);
+    tmp_cofactor_copy = zeros(0,1);
+    tmp_pdbid = cell(0,1);
+    
+    for j = 1:length(loc_tmp)
+        
+        protein_stoic = pdb_processing_1.prot_stoic(loc_tmp(j));
+        
+        tmppdbid = pdb_processing_1.pdbid(loc_tmp(j));
+        tmptype = pdb_processing_1.cofactor{loc_tmp(j)};
+        tmpcopy = pdb_processing_1.cofactor_stoic{loc_tmp(j)};
+        
+        tmptype = strsplit(tmptype,'; ')';
+        tmpcopy = strsplit(tmpcopy,'; ')';
+        naidx = ismember(tmptype,'NA'); % remove NA data
+        tmptype = tmptype(~naidx);
+        tmpcopy = tmpcopy(~naidx);
+        
+        if ~isempty(tmpcopy)
+            tmpcopy = str2num(cell2mat(tmpcopy))/protein_stoic;
+            tmp_cofactor_type = [tmp_cofactor_type;tmptype];
+            tmp_cofactor_copy = [tmp_cofactor_copy;tmpcopy];
+            tmp_pdbid = [tmp_pdbid;repelem(tmppdbid,length(tmptype))'];
         end
-        % select min evalue
-%         idx_cmp_tmp = cmp_num_tmp(:,4) == max(cmp_num_tmp(:,4)) & cmp_num_tmp(:,3) == min(cmp_num_tmp(:,3));
-        idx_cmp_tmp = cmp_num_tmp(:,3) == min(cmp_num_tmp(:,3));
-        strlist_tmp = cmp_txt_tmp(idx_cmp_tmp);
-        strlist_tmp = unique(cellfun(@(x) x(1:4),strlist_tmp,'UniformOutput',false));
-        idx_tmp_tmp = ismember(pdb_processing_1.pdbid,strlist_tmp) & ismember(pdb_processing_1.geneid,gene_tmp);
-        tmp_pdbid = pdb_processing_1.pdbid(idx_tmp_tmp);
-        tmp_cofactor = pdb_processing_1.cofactor(idx_tmp_tmp);
-        tmp_prot_stoic = pdb_processing_1.prot_stoic(idx_tmp_tmp);
-        tmp_cofactor_stoic = pdb_processing_1.cofactor_stoic(idx_tmp_tmp);
-        unique_prot_stoic = unique(tmp_prot_stoic);
-        for j = 1:length(unique_prot_stoic)
-            tmp_idx = tmp_prot_stoic == unique_prot_stoic(j);
-            pdb_processing_2.pdbid = [pdb_processing_2.pdbid;{strjoin(tmp_pdbid(tmp_idx),'/')}];
-            pdb_processing_2.geneid = [pdb_processing_2.geneid;gene_tmp];
-            pdb_processing_2.prot_stoic = [pdb_processing_2.prot_stoic;unique_prot_stoic(j)];
-            pdb_processing_2.cofactor = [pdb_processing_2.cofactor;{strjoin(tmp_cofactor(tmp_idx),'/')}];
-            pdb_processing_2.cofactor_stoic = [pdb_processing_2.cofactor_stoic;{strjoin(tmp_cofactor_stoic(tmp_idx),'/')}];
+    end
+    
+    if ~isempty(tmp_cofactor_type)
+        unique_cofactor_type = unique(tmp_cofactor_type);
+        unique_cofactor_copy = cell(length(unique_cofactor_type),1);
+        unique_pdbid = cell(length(unique_cofactor_type),1);
+
+        for j = 1:length(unique_cofactor_type)
+            tmptype = unique_cofactor_type(j);
+            tmpidx = ismember(tmp_cofactor_type,tmptype);
+            tmpcopy = tmp_cofactor_copy(tmpidx);
+            tmppdbid = tmp_pdbid(tmpidx);
+            idx_new = tmpcopy == max(tmpcopy);
+            pdbid_new = tmppdbid(idx_new);
+            pdbid_new = {strjoin(pdbid_new,'/')};
+            unique_cofactor_copy(j) = {num2str(max(tmpcopy))}; 
+            unique_pdbid(j) = pdbid_new;
         end
+        pdb_cofactor.protein = [pdb_cofactor.protein;gene_tmp];
+        pdb_cofactor.cofactor_type = [pdb_cofactor.cofactor_type;{strjoin(unique_cofactor_type,'; ')}];
+        pdb_cofactor.cofactor_copy = [pdb_cofactor.cofactor_copy;{strjoin(unique_cofactor_copy,'; ')}];
+        pdb_cofactor.pdbid = [pdb_cofactor.pdbid;{strjoin(unique_pdbid,'; ')}];
     end
 end
 
-save('pdb_processing_2.mat','pdb_processing_2');
+save('pdb_cofactor.mat','pdb_cofactor');
 
 
 
-i=128
+% 3. Collect protein stoichiometry information
+
+genes_collected = unique(pdb_processing_1.geneid);
+blastp_all_pdb = strcat(blastp.pdb,':',blastp.protein);
+
+pdb_protein_stoichiometry = struct();
+pdb_protein_stoichiometry.pdbid = cell(0,1);
+pdb_protein_stoichiometry.protein = cell(0,1);
+pdb_protein_stoichiometry.prot_stoic = zeros(0,1); % number of peptides per complex
+
+for i = 1:length(genes_collected)
+    disp(['Collecting protein stoichiometry: ' num2str(i) '/' num2str(length(genes_collected))]);
+    gene_tmp = genes_collected(i);
+    idx_tmp = ismember(pdb_processing_1.geneid,gene_tmp);
+	loc_tmp = find(idx_tmp);
+    
+    cmp_num_tmp = zeros(0,4);
+    cmp_txt_tmp = cell(0,1);
+    for j = 1:length(loc_tmp)
+        pdbid_tmp = pdb_processing_1.pdbid(loc_tmp(j));
+        pdbchain_tmp = pdb_processing_1.all_pdb_chains(loc_tmp(j));
+        pdbchain_tmp = split(pdbchain_tmp,'; ');
+        for k = 1:length(pdbchain_tmp)
+            str_tmp = strcat(pdbid_tmp,'_',pdbchain_tmp(k),':',gene_tmp);
+            if ismember(str_tmp,blastp_all_pdb)
+                idx_blastp_tmp = ismember(blastp_all_pdb,str_tmp);
+                n_tmp = length(find(idx_blastp_tmp));
+                identity_tmp = blastp.identity(idx_blastp_tmp);
+                coverage_tmp = blastp.coverage(idx_blastp_tmp);
+                evalue_tmp = blastp.evalue(idx_blastp_tmp);
+                bit_tmp = blastp.bit(idx_blastp_tmp);
+                row_tmp = [identity_tmp coverage_tmp evalue_tmp bit_tmp];
+                cmp_num_tmp = [cmp_num_tmp;row_tmp];
+                cmp_txt_tmp = [cmp_txt_tmp;repelem(str_tmp,n_tmp)'];
+            else
+                cmp_num_tmp = [cmp_num_tmp;[0 0 1000 0]];
+                cmp_txt_tmp = [cmp_txt_tmp;str_tmp];
+            end
+        end
+    end
+    
+    % select max coverage
+    idx_cmp_tmp = cmp_num_tmp(:,2) == max(cmp_num_tmp(:,2));
+    strlist_tmp = cmp_txt_tmp(idx_cmp_tmp);
+    strlist_tmp = unique(cellfun(@(x) x(1:4),strlist_tmp,'UniformOutput',false));
+    idx_tmp_tmp = ismember(pdb_processing_1.pdbid,strlist_tmp) & ismember(pdb_processing_1.geneid,gene_tmp);
+    tmp_pdbid = pdb_processing_1.pdbid(idx_tmp_tmp);
+    tmp_prot_stoic = pdb_processing_1.prot_stoic(idx_tmp_tmp);
+    
+    unique_prot_stoic = unique(tmp_prot_stoic);
+    unique_pdbid = cell(length(unique_prot_stoic),1);
+    for j = 1:length(unique_prot_stoic)
+        tmp_pdbid_list = tmp_pdbid(tmp_prot_stoic == unique_prot_stoic(j));
+        tmp_pdbid_list = unique(tmp_pdbid_list);
+        unique_pdbid(j) = {strjoin(tmp_pdbid_list,'/')};
+    end
+    
+    pdb_protein_stoichiometry.protein = [pdb_protein_stoichiometry.protein;repelem(gene_tmp,length(unique_prot_stoic))'];
+    pdb_protein_stoichiometry.prot_stoic = [pdb_protein_stoichiometry.prot_stoic;unique_prot_stoic];
+    pdb_protein_stoichiometry.pdbid = [pdb_protein_stoichiometry.pdbid;unique_pdbid];
+    
+end
+
+save('pdb_protein_stoichiometry.mat','pdb_protein_stoichiometry');
 
 toc;
 
