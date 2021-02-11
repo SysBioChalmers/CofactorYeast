921a4691761eb7b3f6a46157ad9760c1821fadba
Yu-sysbio <cheyu@chalmers.se>
2021-02-11 09:49:02 +0100

    Remove files

--- Noscapine/constructNoscapine.m
+++
@@ -1,7 +0,0 @@
-%% construct noscapine pathway
-load('CofactorYeast.mat');
-load('enzymedata.mat');
-[model,enzymedata] = addNewPathway(model,enzymedata,'New_pathway_noscapine.xlsx');
-[model,enzymedata] = expandModel(model,enzymedata);
-save('modelNoscapine.mat','model');
-save('enzymedataNoscapine.mat','enzymedata');
